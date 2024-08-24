65f58ccd0d2e4f42c834fe34223875e485b01a7b
MayraVarela <Mayravarelaparra16@gmail.com>
2024-08-25 00:26:56 +0200

    redux config

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
-import app from "./App.css"
+import { BrowserRouter, Navigate, Routes, Route} from 'react-router-dom'
+import HomePage from './scenes/homePage/Index'
+import LoginPage from './scenes/loginPage/index'
+import ProfilePage from './scenes/profilePage/Index';
+
 
 function App() {
+
+  
   return (
     <div className="app">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/scenes/profilePage/Index.jsx
@@ -0,0 +1,8 @@
+
+const ProfilePage = () => {
+    return (
+        <div>ProfileePage</div>
+    )
+}
+
+export default ProfilePage
