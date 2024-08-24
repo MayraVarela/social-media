8b5888969d069d5d592f578e96bed7e9cce1a058
MayraVarela <Mayravarelaparra16@gmail.com>
2024-08-25 01:45:18 +0200

    styling setup

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,10 @@
+import { box } from "@mui/material";
+import { styled } from "@mui/material"
+
+const FlexBetween = styled(box)({
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center"
+})
+
+export default FlexBetween;
--- client/src/scenes/navBar/Index.jsx
+++ client/src/scenes/navBar/Index.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+import { box, 
+    IconButton, 
+    InputBase,
+    Typography,
+    Select,
+    MenuItem,
+    FormControl,
+    useTheme,
+    useMediaQuery } from "@mui/material";
+    import {
+        Search,
+        Message,
+        DarkMode,
+        LightMode,
+        Notifications,
+        Help,
+        Menu,
+        Close
+    } from "@mui/icons-material"
+    import { useDispatch, useSelector } from "react-redux";
+    import { setMode, setLogout } from "state";
+    import { useNavigate } from "react-router-dom"
+    import FlexBetween from "components/FlexBetween";
+
 
 const Navbar = () => {
+    const [isMobileMenuTogggled, setIsMobileMenuTogggled] = useState(false);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const user = useSelector((state) => state.user);
+    const isNonMobileScreens = useMediaQuery()
     return (
         <div>Navbar</div>
     )
